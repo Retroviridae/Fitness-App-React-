8358ebb45dccf0aeb44a6a738c8c0b804432aca7
Louis Y <louisy603@hotmail.com>
2022-05-09 11:18:28 -0400

    form-layout

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -4,14 +4,19 @@ function Form() {
   return (
     <div>
         <form>
-            <input type="date" name="date"/>
+            <input type="date" name="date" />
             
-            <input type="text" name="workout"/>
+            <input type="text" name="workout" placeholder="Workout"/>
+            
             <select>
                 <option value="Cardio">Cardio</option>
                 <option value="Weights">Weights</option>
                 <option value="Sports">Sports</option>
             </select>
+
+            <input type="number" name="calories" placeholder="Calories"/>
+            <input type="Details" name="Details" placeholder="Details"/>
+
         </form>
     </div>
   )
