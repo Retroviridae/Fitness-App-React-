397437b1e7ca182c6f9c16d2028ab7ea43aca326
Louis Y <louisy603@hotmail.com>
2022-05-09 17:38:21 -0400

    added-fetch

--- my-app/src/WorkoutList.js
+++ my-app/src/WorkoutList.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Workout from "./Workout";
 
-function WorkoutList() {
+function WorkoutList({workoutArr}) {
+  // const workoutMaker = workoutArr.map(workOut => {
+  //   return (
+  //     <Workout
+  //     key={workOut.id}
+  //     workOut={workOut} 
+  //     />
+  //   )
+  // })
   return (
       <div>
           <Workout />
+          
       </div>
   );
 }
