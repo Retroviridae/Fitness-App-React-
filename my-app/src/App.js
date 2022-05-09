ef80ba02f28314082cdaf3048426a770533d4933
Alexander Schuessler <schuessler.alex@gmail.com>
2022-05-09 10:53:35 -0500

    created workoutList component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,26 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Workout from './Workout';
+import Form from './Form';
+import WorkoutList from './WorkoutList';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Workout />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
+      <WorkoutList />
     </div>
   );
 }
