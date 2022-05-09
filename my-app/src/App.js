e3ea7fd835199f11856c163a6e505e4578510822
Louis Y <louisy603@hotmail.com>
2022-05-09 12:12:38 -0400

    moved-db-to-root

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Workout from './Workout';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  
+  useEffect(() => {
+    fetch("http://localhost:3000/workouts")
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
   return (
     <div className="App">
       <Header />
