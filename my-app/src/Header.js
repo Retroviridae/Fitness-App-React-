57efcf6a8f7cd61da31c728526c069cb19ced9ba
Louis Y <louisy603@hotmail.com>
2022-05-10 16:39:13 -0400

    push

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -21,9 +21,9 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/workouts">Workouts</Nav.Link>
-            <Nav.Link href="/new">New</Nav.Link>
+            <NavLink  exact to="/">Home</NavLink>
+            <NavLink to="/workouts">Workouts</NavLink>
+            <NavLink to="/new">New</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
