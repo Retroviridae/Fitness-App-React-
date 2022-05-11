ff9e7403a635d9c5ce18dd51eb9af7dc050204e3
Alexander Schuessler <schuessler.alex@gmail.com>
2022-05-11 13:34:14 -0500

    Fixed alignment of a few components.

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,17 +3,17 @@ import { Card, Button, ListGroup, Form } from 'react-bootstrap/';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function Home({ goals }) {
+function Home() {
   return (
-      <div>
-        <Card border="primary" style={{ width: '40rem' }}>
+      <div >
+        <Card border="primary" style={{ width: '40rem',margin: 'auto' }}>
         <Card.Header className="text-center">Goals</Card.Header>
         <Card.Img variant="top" src="https://ih1.redbubble.net/image.1000561856.0547/pp,840x830-pad,1000x1000,f8f8f8.jpg" />
             <Card.Body>
                 <ListGroup>
-                    <ListGroup.Item>Goal Weight: </ListGroup.Item>
-                    <ListGroup.Item>Primary goal:  </ListGroup.Item>
-                    <ListGroup.Item>Seconday goal:  </ListGroup.Item>
+                    <ListGroup.Item>Goal Weight: 235lbs </ListGroup.Item>
+                    <ListGroup.Item>Primary goal: Be promoted to CEO </ListGroup.Item>
+                    <ListGroup.Item>Seconday goal: Marry Gretchen instead of Angela   </ListGroup.Item>
                 </ListGroup>
                 <Button variant="primary" href="/edit">Edit</Button>
             </Card.Body>    
